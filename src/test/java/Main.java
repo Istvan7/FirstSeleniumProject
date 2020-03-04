a5930c8267f355fd80f3064bff3522f0a32f2bda
Feher Istvan <54443473+Istvan7@users.noreply.github.com>
2020-03-04 18:24:50 +0200

    a

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,18 +2,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        RegisterTest reg = new RegisterTest();
-        LoginTest login = new LoginTest();
-        reg.register();
-        login.login();
-        WishListTest wishl = new WishListTest();
-        wishl.wishList();
 
 
 
 
     }
-}
+
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -4,16 +4,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegisterTest {
 
-    public  void register() {
-
+    public static void registerTest() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
         driver.get("http://testfasttrackit.info/selenium-test/");
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.cssSelector("#send2")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector("#firstname")).sendKeys("Dorel");
+        driver.findElement(By.cssSelector("#middlename")).sendKeys("Dorel");
+        driver.findElement(By.cssSelector("#lastname")).sendKeys("Cristurean");
+        driver.findElement(By.cssSelector("#email_address")).sendKeys("dorel_c@mailinator.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("123456789");
+        driver.findElement(By.cssSelector("#confirmation")).sendKeys("123456789");
+        driver.findElement(By.cssSelector("#is_subscribed")).click();
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
         driver.quit();
+
+
 
 
     }
